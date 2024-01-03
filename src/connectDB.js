1a125bd0cc4170560e46a4c5e6ee0f5e97445a57
ompateldeveloper <ompatel.developer@gmail.com>
2024-01-03 17:07:02 +0530

    changed start command

--- src/connectDB.js
+++ src/connectDB.js
@@ -1,6 +1,6 @@
 import {connect} from "mongoose";
 export default function connectDB(){
-    connect(process.env.URI || "idk what to connect anymore")
+    connect(process.env.MONGO_URI || "idk what to connect anymore")
     console.log('database connected');
 
 }
